dffa23dc5e329c4881f18601774ec356b9faebc2
Zhodong Liu <zl4789@nyu.edu>
2025-06-13 06:15:10 -0400

    add sh

---
+++ train_decoder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --output=jobs/Job.%j.out
+#SBATCH --error=jobs/Job.%j.err
+#SBATCH --partition=rtx8000,v100
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=32GB
+#SBATCH --time=6:00:00
+#SBATCH --gres=gpu:1      
+#SBATCH --mail-type=ALL          
+#SBATCH --mail-user=zl4789@nyu.edu
+#SBATCH --requeue
+
+source /share/apps/anaconda3/2020.07/etc/profile.d/conda.sh;
+conda activate TIGER
+python3 train_rqvae.py configs/rqvae_ml32m.gin
+conda deactivate
